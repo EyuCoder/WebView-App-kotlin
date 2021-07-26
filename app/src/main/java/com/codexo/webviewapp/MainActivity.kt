83b9141cf4a70ab2c9f9739c8508ba28bdeca352
elysium09 <eyueldaniel0921@gmail.com>
2021-07-26 19:16:51 +0300

    :hammer: dialog progress added

--- app/src/main/java/com/codexo/webviewapp/MainActivity.kt
+++ app/src/main/java/com/codexo/webviewapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.codexo.webviewapp
 
 import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
@@ -16,19 +20,27 @@ import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
-    private val mainUrl = "https://zenachmart.com"
+    private val mainUrl = "https://ebstv.tv/"
     private lateinit var webView: WebView
     private lateinit var progressBar: ProgressBar
     private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var customDialog: Dialog
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         webView = findViewById(R.id.wv_main)
-        progressBar = findViewById(R.id.pb_main)
+//        progressBar = findViewById(R.id.pb_dialog)
+//        progressBar.max = 100
         swipeRefresh = findViewById(R.id.sr_main)
-        progressBar.max = 100
+
+        customDialog = Dialog(this)
+//        customDialog = Dialog(this, R.style.dialogTransparent)
+        customDialog.setContentView(R.layout.dialog_lottie)
+        customDialog.setCancelable(false)
+        customDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
 
         webView.webViewClient = MyWVClient()
         webView.loadUrl(mainUrl)
@@ -46,15 +58,19 @@ class MainActivity : AppCompatActivity() {
             webView.webChromeClient = object : WebChromeClient() {
                 override fun onProgressChanged(view: WebView, newProgress: Int) {
                     super.onProgressChanged(view, newProgress)
-                    progressBar.progress = newProgress
-                    if (newProgress < 100 && progressBar.visibility == ProgressBar.GONE) {
-                        progressBar.visibility = ProgressBar.VISIBLE
+//                    progressBar.progress = newProgress
+//                    if (newProgress < 100 && progressBar.visibility == ProgressBar.GONE) {
+                    if (newProgress < 100) {
+                        customDialog.show()
+//                        progressBar.visibility = ProgressBar.VISIBLE
                     }
                     if (newProgress == 100) {
-                        progressBar.visibility = ProgressBar.GONE
+                        customDialog.dismiss()
+//                        progressBar.visibility = ProgressBar.GONE
                         swipeRefresh.isRefreshing = false
                     } else {
-                        progressBar.visibility = ProgressBar.VISIBLE
+                        customDialog.show()
+//                        progressBar.visibility = ProgressBar.VISIBLE
                     }
                 }
             }
@@ -75,11 +91,14 @@ class MainActivity : AppCompatActivity() {
 
     inner class MyWVClient : WebViewClient() {
         override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-            val targetUrl = "zenachmart.com"
-            if (Uri.parse(url).host == targetUrl) {
+            val targetUrl1 = "www.ebstv.tv"
+            val targetUrl2 = "ebstv.tv/"
+            val targetUrl3 = ""
+            if (Uri.parse(url).host == targetUrl1 || Uri.parse(url).host == targetUrl2) {
                 webView.loadUrl(url)
-                Toast.makeText(baseContext, Uri.parse(url).host, Toast.LENGTH_LONG).show()
+                //Toast.makeText(baseContext, Uri.parse(url).host, Toast.LENGTH_LONG).show()
             } else {
+                Toast.makeText(this@MainActivity, url, Toast.LENGTH_LONG).show()
                 val i = Intent(Intent.ACTION_VIEW)
                 i.data = Uri.parse(url)
                 startActivity(i)
